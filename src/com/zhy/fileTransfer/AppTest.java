1db3e98a9b3eeba52bbd52b3318ea281372122f2
zhouhaoyan <ttzhyran@163.com>
2017-01-24 23:27:38 +0800

    Initial commit

---
+++ src/com/zhy/fileTransfer/AppTest.java
@@ -0,0 +1,7 @@
+package com.zhy.fileTransfer;
+
+/**
+ * Created by ttzhy on 2017/1/24.
+ */
+public class AppTest {
+}
---
+++ src/com/zhy/fileTransfer/fileServer/FileServer.java
@@ -0,0 +1,12 @@
+package com.zhy.fileTransfer.fileServer;
+
+/**
+ * Created by zhouhaoyan on 2017/1/24.
+ * Project name: fileManager
+ * Package name: com.zhy.fileTransfer.fileServer
+ * Description:
+ */
+public class FileServer {
+
+
+}
